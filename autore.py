d117b7a7aa73f1bd5c2e673ceda407c9f9931f1d
Roy <roy.liang@ectrend.com>
2018-03-08 11:23:47 +0800

    update autosend

--- autore.py
+++ autore.py
@@ -70,121 +70,121 @@ def auto_gen_quote_pic():
 
 	for m in mq_last:
 		if m['name'] == u'氧化镨钕':
-			r0_l = m['avg_quote']
+			r0_l = int(m['avg_quote'])
 			draw.text((445,80),str(r0_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'镨钕金属':
-			r1_l = m['avg_quote']
+			r1_l = int(m['avg_quote'])
 			draw.text((445,116),str(r1_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化镧':
-			r2_l = m['avg_quote']
+			r2_l = int(m['avg_quote'])
 			draw.text((445,156),str(r2_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属镧':
-			r3_l = m['avg_quote']
+			r3_l = int(m['avg_quote'])
 			draw.text((445,196),str(r3_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化铈':
-			r4_l = m['avg_quote']
+			r4_l = int(m['avg_quote'])
 			draw.text((445,235),str(r4_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属铈':
-			r5_l = m['avg_quote']
+			r5_l = int(m['avg_quote'])
 			draw.text((445,270),str(r5_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化铽':
-			r6_l = m['avg_quote']
+			r6_l = int(m['avg_quote'])
 			draw.text((445,308),str(r6_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属铽':
-			r7_l = m['avg_quote']
+			r7_l = int(m['avg_quote'])
 			draw.text((445,348),str(r7_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化镝' and r8_l == '':
-			r8_l = m['avg_quote']
+			r8_l = int(m['avg_quote'])
 			draw.text((445,385),str(r8_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属镝':
-			r9_l = m['avg_quote']
+			r9_l = int(m['avg_quote'])
 			draw.text((445,425),str(r9_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'镧铈金属':
-			r10_l = m['avg_quote']
+			r10_l = int(m['avg_quote'])
 			draw.text((445,462),str(r10_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化镨':
-			r11_l = m['avg_quote']
+			r11_l = int(m['avg_quote'])
 			draw.text((445,500),str(r11_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属镨' and r12_l == '':
-			r12_l = m['avg_quote']
+			r12_l = int(m['avg_quote'])
 			draw.text((445,538),str(r12_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化钕':
-			r13_l = m['avg_quote']
+			r13_l = int(m['avg_quote'])
 			draw.text((445,575),str(r13_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属钕':
-			r14_l = m['avg_quote']
+			r14_l = int(m['avg_quote'])
 			draw.text((445,610),str(r14_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化钐':
-			r15_l = m['avg_quote']
+			r15_l = int(m['avg_quote'])
 			draw.text((445,648),str(r15_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属钐':
-			r16_l = m['avg_quote']
+			r16_l = int(m['avg_quote'])
 			draw.text((445,685),str(r16_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'钆铁':
-			r17_l = m['avg_quote']
+			r17_l = int(m['avg_quote'])
 			draw.text((445,723),str(r17_l),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'镝铁' and r18_l =='':
-			r18_l = m['avg_quote']
+			r18_l = int(m['avg_quote'])
 			draw.text((445,760),str(r18_l),fill=(0,0,0),font=ttfont)
 
 
 	for m in mc_current:
 		if m['name'] == u'氧化镨钕':
-			r0_c = m['avg_quote']
+			r0_c = int(m['avg_quote'])
 			draw.text((620,80),str(r0_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'镨钕金属':
-			r1_c = m['avg_quote']
+			r1_c = int(m['avg_quote'])
 			draw.text((620,116),str(r1_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化镧':
-			r2_c = m['avg_quote']
+			r2_c = int(m['avg_quote'])
 			draw.text((620,156),str(r2_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属镧':
-			r3_c = m['avg_quote']
+			r3_c = int(m['avg_quote'])
 			draw.text((620,196),str(r3_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化铈':
-			r4_c = m['avg_quote']
+			r4_c = int(m['avg_quote'])
 			draw.text((620,235),str(r4_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属铈':
-			r5_c = m['avg_quote']
+			r5_c = int(m['avg_quote'])
 			draw.text((620,270),str(r5_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化铽':
-			r6_c = m['avg_quote']
+			r6_c = int(m['avg_quote'])
 			draw.text((620,308),str(r6_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属铽':
-			r7_c = m['avg_quote']
+			r7_c = int(m['avg_quote'])
 			draw.text((620,348),str(r7_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化镝' and r8_c == '':
-			r8_c = m['avg_quote']
+			r8_c = int(m['avg_quote'])
 			draw.text((620,385),str(r8_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属镝':
-			r9_c = m['avg_quote']
+			r9_c = int(m['avg_quote'])
 			draw.text((620,425),str(r9_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'镧铈金属':
-			r10_c = m['avg_quote']
+			r10_c = int(m['avg_quote'])
 			draw.text((620,462),str(r10_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化镨':
-			r11_c = m['avg_quote']
+			r11_c = int(m['avg_quote'])
 			draw.text((620,500),str(r11_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属镨' and r12_c=='':
-			r12_c = m['avg_quote']
+			r12_c = int(m['avg_quote'])
 			draw.text((620,538),str(r12_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化钕':
-			r13_c = m['avg_quote']
+			r13_c = int(m['avg_quote'])
 			draw.text((620,575),str(r13_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属钕':
-			r14_c = m['avg_quote']
+			r14_c = int(m['avg_quote'])
 			draw.text((620,610),str(r14_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'氧化钐':
-			r15_c = m['avg_quote']
+			r15_c = int(m['avg_quote'])
 			draw.text((620,648),str(r15_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'金属钐':
-			r16_c = m['avg_quote']
+			r16_c = int(m['avg_quote'])
 			draw.text((620,685),str(r16_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'钆铁':
-			r17_c = m['avg_quote']
+			r17_c = int(m['avg_quote'])
 			draw.text((620,723),str(r17_c),fill=(0,0,0),font=ttfont)
 		elif m['name'] == u'镝铁' and r18_c=='':
-			r18_c = m['avg_quote']
+			r18_c = int(m['avg_quote'])
 			draw.text((620,760),str(r18_c),fill=(0,0,0),font=ttfont)
 
 	diff = '-' if r0_c-r0_l==0 else r0_c-r0_l
@@ -268,9 +268,8 @@ def auto_gen_quote_pic():
 	#im.close()
 
 	#test_file = u'~/Downloads/000003.gif'
-	client = WeChatClient('wx62c42eca1fa1c67d', '8ab2a57cf2b04fbd2fb9a6db66eaee95')
-	image = client.media.upload('image',open(os.path.expanduser(save_filename),'r'))
-	
+	#client = WeChatClient('wx62c42eca1fa1c67d', '8ab2a57cf2b04fbd2fb9a6db66eaee95')
+	#image = client.material.add('image',open(os.path.expanduser(save_filename),'r'))
 	#print client.message.send_mass_image(None,image['media_id'],True)
 
 if __name__ == "__main__":
